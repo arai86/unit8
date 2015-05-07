01e484c181ba4f3da7d967b2a992f3a65cb20187
arai86 <felipe.otero@hotmail.es>
2015-05-08 00:21:13 +0200

    clase Alumno commit3

--- unit8/src/unit8/Persona.java
+++ unit8/src/unit8/Persona.java
@@ -1,5 +1,7 @@
 package unit8;
 
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +12,53 @@ package unit8;
  *
  * @author Felipe
  */
-public class Persona {
+public abstract class Persona 
+{
+    protected String nombre;
+    protected String dni;
+    protected ArrayList<Asignatura> asignatura;
+    public Persona(String nombre,String dni)
+    {
+    }
+    public static void anyadirAsignatura(Asignatura a)
+    {
+    }
+    public boolean esProfesor()
+    {
+        return true;
+    }
+    public void mostrarListadoAsignaturasNota()
+    {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public ArrayList<Asignatura> getAsignatura() {
+        return asignatura;
+    }
+
+    public void setAsignatura(ArrayList<Asignatura> asignatura) {
+        this.asignatura = asignatura;
+    }
+    public void ordenarPorAsignaturas()
+    {
+    }
+    public void ordenarPorNota()
+    {
+    }
     
 }
